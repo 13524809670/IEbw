6cc87f1d5b6e3085fea1d4869d1a9d9871d7bcbd
wangrui <1634208912@qq.com>
2016-10-10 23:18:05 +0800

    one

--- demo1/js/banner-one.js
+++ demo1/js/banner-one.js
@@ -1,170 +1,103 @@
-(function(a) {
-    a.fn.DB_tabMotionBanner = function(b) {
-        var c = {
-            key: "",
-            autoRollingTime: 3000,
-            bgSpeed: 1000,
-            motion: ""
-        };
-        a.extend(c, b);
-        return this.each(function() {
-            var h = a(this);
-            var k = h.find(".DB_imgSet");
-            var r = h.find(".DB_imgSet li");
-            var i = h.find(".DB_menuSet");
-            var m = h.find(".DB_menuSet li");
-            var e = h.find(".DB_bgSet li");
-            var q = h.find(".DB_next");
-            var g = h.find(".DB_prev");
-            var f = r.length;
-            var p = 0;
-            var j = 0;
-            s();
-            function s() {
+//作者：jujenjie
+//请尊重作者，可用于商业，请保留此段话
+
+$(function(){
+    var Big_Slide_boxWidth=$("#Big_Slide").width();//获取幻灯片外部div宽度
+    var Big_Slide_boxHeight=$("#Big_Slide").height();//获取幻灯片外部div高度
+    var Big_Slide_LiWidth=$("#Big_Slide").children("ul").children("li").eq(0).width();//获取幻灯片Li的宽度
+    var Big_Slide_liNubr=$("#Big_Slide").find('li').length;//获取幻灯片Li的数量
+    var Big_Slide_Speed=3000;//滚动速度
+    var Big_Slide_Tab_Contne="";//初始化tab按钮
+    var Big_Slide_Last_NextHeight=$("#Big_Slide_Last").height();//获取按钮高度
+    //tab 条宽度
+    var Big_Slide_Tab_AWidth=(30/Big_Slide_liNubr)*2;
+    var Big_Slide_TabWidth=$("#Big_Slide_Tab").width();//tab的宽度
+    var Big_SlideWidth=$("#Big_Slide").width();//Big_Slide的宽度
+    $("#Big_Slide_Tab").css("left",(Big_SlideWidth-Big_Slide_TabWidth)*0.5);//Big_Slide_Tab定位
+    $("#Big_Slide_Last").css("top",(Big_Slide_boxHeight-Big_Slide_Last_NextHeight)*0.5);
+    $("#Big_Slide_Next").css("top",(Big_Slide_boxHeight-Big_Slide_Last_NextHeight)*0.5);
+    $("#prevL").css("left",-Big_Slide_LiWidth);
+    $("#prevR").css("right",-Big_Slide_LiWidth);
+    
+    for(var i=0;i<parseInt(Big_Slide_liNubr);i++){
+        $("#Big_Slide").children("ul").children("li").eq(i).css("left",(i-1)*Big_Slide_LiWidth);//初始化Li位置
+        
+        if(i==1){
+            Big_Slide_Tab_Contne=Big_Slide_Tab_Contne+"<a class='"+"TabOn' id='"+"TabOn"+i+"' style='width:"+Big_Slide_Tab_AWidth+"px'></a>";
+            }else{
+                Big_Slide_Tab_Contne=Big_Slide_Tab_Contne+"<a id='TabOn"+i+"' style='width:"+Big_Slide_Tab_AWidth+"px'></a>";//生成tab按钮
+                }
+        }
+    $("#Big_Slide_Tab").html(Big_Slide_Tab_Contne);//写入tab按钮
+    
+    var Slide_Run = setInterval(Slide_Next,Big_Slide_Speed)//设置滚动器
+    
+    function Slide_Next(){
+        for(var k=0;k<parseInt(Big_Slide_liNubr);k++){
+            if(parseInt($("#Big_Slide").children("ul").children("li").eq(k).css("left"))==-Big_Slide_LiWidth)//判断LI是否有位移到0，防止同时多次点击出错
+            {
                 
-                l();
-                d();
-                t();
-                o()
-            }
-            function l() {
-                k.css({
-                    position: "relative"
-                });
-                r.css({
-                    position: "absolute"
-                });
-                for (var y = 0; y < f; y++) {
-                    if (y == p) {
-                        r.eq(y).show()
-                    } else {
-                        r.eq(y).hide()
-                    }
-                }
-                for (var y = 0; y < r.length; y++) {
-                    var x = r.eq(y).find("img");
-                    for (var w = 0; w < x.length; w++) {
-                        var A = x.eq(w);
-                        var v = c.motion[A.attr("class")];
-                        if (v != null) {
-                            var z = Number(A.css("left").split("px")[0]);
-                            var B = Number(A.css("top").split("px")[0]);
-                            A.data({
-                                x2: z,
-                                y2: B,
-                                x1: z + v.left,
-                                y1: B + v.top,
-                                opacity: v.opacity,
-                                speed: v.speed,
-                                delay: v.delay == null ? 0 : v.delay
-                            })
+                var Big_Slide_liSeat=0;//位置参数归零
+                for(var j=0;j<parseInt(Big_Slide_liNubr);j++){
+                    if(parseInt($("#Big_Slide").children("ul").children("li").eq(j).css("left"))==-Big_Slide_LiWidth){//判断是否第一个
+                        
+                        $("#Big_Slide").children("ul").children("li").eq(j).css("left",Big_Slide_LiWidth*(Big_Slide_liNubr-2));//第一个回到最后一个
+
+                        }else{
+                            
+                        Big_Slide_liSeat=parseInt($("#Big_Slide").children("ul").children("li").eq(j).css("left"))-Big_Slide_LiWidth;//获取位移位置
+                        $("#Big_Slide").children("ul").children("li").eq(j).animate({left:Big_Slide_liSeat},"slow");//进行位移动画
+
                         }
                     }
+                    
                 }
             }
-            function d() {
-                h.bind("mouseenter",
-                function() {
-                    clearInterval(j);
-                    q.show();
-                    g.show()
-                });
-                h.bind("mouseleave",
-                function() {
-                    t();
-                    q.hide();
-                    g.hide()
-                });
-                m.bind("click",
-                function() {
-                    if (a(this).index() != p) {
-                        p = a(this).index();
-                        o()
-                    }
-                });
-                m.bind("mouseenter",
-                function() {
-                    n(a(this).find("img"), "src", "_cir1", "_cir2")
-                });
-                m.bind("mouseleave",
-                function() {
-                    if (p != a(this).index()) {
-                        n(a(this).find("img"), "src", "_cir2", "_cir1")
-                    }
-                });
-                q.bind("click",
-                function() {
-                    u()
-                });
-                g.bind("click",
-                function() {
-                    p--;
-                    if (p == -1) {
-                        p = f - 1
-                    }
-                    o()
-                })
-            }
-            function u() {
-                p = ++p % f;
-                o()
-            }
-            function t() {
-                j = setInterval(u, c.autoRollingTime)
-            }
-            function o() {
-                for (var z = 0; z < r.length; z++) {
-                    var A = r.eq(z);
-                    var y = e.eq(z);
-                    if (p == z) {
-                        A.show();
-                        var x = A.find("img");
-                        for (var w = 0; w < x.length; w++) {
-                            var A = x.eq(w);
-                            var v = c.motion[A.attr("class")];
-                            if (v != null) {
-                                if (A.attr("src").indexOf(".png") > 0 && a.browser.msie && a.browser.version < 9) {
-                                    A.css({
-                                        left: A.data("x1"),
-                                        top: A.data("y1"),
-                                        opacity: 1,
-                                        display: "none"
-                                    })
-                                } else {
-                                    A.css({
-                                        left: A.data("x1"),
-                                        top: A.data("y1"),
-                                        opacity: A.data("opacity")
-                                    })
-                                }
-                                A.stop().delay(A.data("delay")).queue(function() {
-                                    a(this).css("display", "block");
-                                    a(this).dequeue()
-                                }).animate({
-                                    left: A.data("x2"),
-                                    top: A.data("y2"),
-                                    opacity: 1
-                                },
-                                A.data("speed"))
-                            }
-                        }
-                        n(m.eq(z).find("img"), "src", "_cir1", "_cir2");
-                        m.eq(z).addClass("select");
-                        y.stop(true, true).fadeIn(c.bgSpeed)
-                    } else {
-                        A.hide();
-                        n(m.eq(z).find("img"), "src", "_cir2", "_cir1");
-                        m.eq(z).removeClass("select");
-                        y.stop(true, true).fadeOut(c.bgSpeed)
-                    }
-                }
-            }
-            function n(w, z, v, x) {
-                var y = w.attr(z);
-                if (String(y).search(v) != -1) {
-                    w.attr(z, y.replace(v, x))
-                }
-            }
-        })
     }
-})(jQuery);
+    
+    function Slide_Last(){
+        for(var k=0;k<parseInt(Big_Slide_liNubr);k++){
+            if(parseInt($("#Big_Slide").children("ul").children("li").eq(k).css("left"))==0)//判断LI是否有位移到0，防止同时多次点击出错
+            {
+                
+                var Big_Slide_liSeat=0;//位置参数归零
+                for(var j=0;j<parseInt(Big_Slide_liNubr);j++){
+                    if(parseInt($("#Big_Slide").children("ul").children("li").eq(j).css("left"))==Big_Slide_LiWidth*(Big_Slide_liNubr-2)){//判断是否第一个
+                        
+                        $("#Big_Slide").children("ul").children("li").eq(j).css("left",-Big_Slide_LiWidth);//第一个回到最后一个
+                        
+                        }else{
+                            
+                        Big_Slide_liSeat=parseInt($("#Big_Slide").children("ul").children("li").eq(j).css("left"))+Big_Slide_LiWidth;//获取位移位置
+                        $("#Big_Slide").children("ul").children("li").eq(j).animate({left:Big_Slide_liSeat},"slow");//进行位移动画
+                        
+                        }
+                    }
+                    
+                }
+            }
+    }
+    
+    setInterval(function(){
+        for(var n=0;n<parseInt(Big_Slide_liNubr);n++){
+            if(parseInt($("#Big_Slide").children("ul").children("li").eq(n).css("left"))==0){
+                if((n-1)<0){
+                    $("#TabOn"+(Big_Slide_liNubr-1)).removeClass("TabOn");
+                    $("#TabOn"+(n+1)).removeClass("TabOn");
+                    $("#TabOn"+n).addClass("TabOn");
+                    }else {
+                        $("#TabOn0").removeClass("TabOn");
+                        $("#TabOn"+(n-1)).removeClass("TabOn");
+                        $("#TabOn"+(n+1)).removeClass("TabOn");
+                        $("#TabOn"+n).addClass("TabOn");
+                            }
+                }
+            }
+        },1)
+    
+    $("#Big_Slide_Next").click(Slide_Next);//下一张按钮
+    $("#Big_Slide_Last").click(Slide_Last);//上一张按钮
+    $("#Big_Slide_box").mouseenter(function(){clearInterval(Slide_Run)});//鼠标在幻灯片上，停止滚动
+    $("#Big_Slide_box").mouseleave(function(){Slide_Run = setInterval(Slide_Next,Big_Slide_Speed)})//鼠标不在幻灯哦上，开始滚动
+    
+})      
